4b5146a746d34060bf649e1ad88c627038ff117b
Alaister Leung <70866538+AlaisterLeung@users.noreply.github.com>
2026-02-12 18:54:28 +0800

    Add 2 print lines as report heading

--- src/main/java/Lab1/MyApp.java
+++ src/main/java/Lab1/MyApp.java
@@ -4,6 +4,8 @@ public class MyApp {
     public static void main(String[] args) {
         int b = 2;
         int n = 11;
+        System.out.println("Program Starting");
+        System.out.println("1 + 1 = "+2);
         System.out.println(b+" to power "+n+" = "+ MyLibrary.Power(b,n));
         System.out.println(n+"! = " + MyLibrary.factorial(n));
         System.out.println("Program Ended");
