ceb0687fc94bf5ac140a7170773f393ba285bc0d
Alaister Leung <70866538+AlaisterLeung@users.noreply.github.com>
2026-02-12 18:50:50 +0800

    This is an initial commit for Comp3111LEx Project

---
+++ src/main/java/Lab1/MyApp.java
@@ -0,0 +1,11 @@
+package Lab1;
+
+public class MyApp {
+    public static void main(String[] args) {
+        int b = 2;
+        int n = 11;
+        System.out.println(b+" to power "+n+" = "+ MyLibrary.Power(b,n));
+        System.out.println(n+"! = " + MyLibrary.factorial(n));
+        System.out.println("Program Ended");
+    }
+}
